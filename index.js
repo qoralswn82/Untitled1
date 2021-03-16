9731b568009a901158ba9c3d9b271155747af59d
qoralswn82 <58283656+qoralswn82@users.noreply.github.com>
2021-03-16 23:53:26 +0900

    make view using template engine ejs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const config = require('./config/key')
 // const bodyParser = require('body-Parser')
 const app = express()
 const { User } = require("./models/user");
+var router = require('./router/main')(app);
+
 const port = 3000
 
 const mongoose = require('mongoose')
@@ -16,9 +18,18 @@ mongoose.connect(config.mongoURI,{
 
 
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World! nodemon 적용 / test')
-})
+
+// app.get('/hello', (req, res) => {
+//   res.send('Hello World! nodemon 적용 / test');
+// })
+
+//html 파일 위치 설정
+app.set('views', __dirname + '/views');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+//html 을 ejs 파일로 바꾸는 건가..?
+app.engine('html', require('ejs').renderFile);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -38,7 +49,10 @@ app.use(express.urlencoded({extended: true}));
 	//client 로 통해 받은 데이터를 DB에 저장
 	const user = new User(req.body);
 	
-	
+	console.log(req.body.name);
+	console.log(req.body.email);
+	console.log(req.body.role);
+	console.log(req.body.password);
 	user.save((err,userInfo)=>{
 		if(err) return res.json({success : false, err})
 			return res.status(200).json({
